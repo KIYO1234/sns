f60271345a4d426a7069b8e1651420d6519f43aa
KIYO1234 <getstarted3601@gmail.com>
2021-07-05 17:28:01 +0900

    like機能まで

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import screamReducer from '../features/screams/screamSlice';
+import userReducer from '../features/users/userSlice';
+
+export const store = configureStore({
+    reducer: {
+        screams: screamReducer,
+        users: userReducer,
+    }
+})
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import firebase from 'firebase';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +19,18 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyA-rEhZ5KhnwNOhl2ImrbbK3ApxAmGU0f0",
+  authDomain: "socialape-73b9a.firebaseapp.com",
+  projectId: "socialape-73b9a",
+  storageBucket: "socialape-73b9a.appspot.com",
+  messagingSenderId: "737662390218",
+  appId: "1:737662390218:web:6cf54ab09228ec409c0933",
+  measurementId: "G-36NFY659CC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
